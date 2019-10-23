b361116eda068c3f2a4d7d2f6ef01267c68d16d0
Ryota Kayanuma <picosushi12+git@gmail.com>
2019-10-23 16:00:24 +0900

    Fix many mistakes, change orders

--- environments/shadow-mistress/templates/user_data.sh
+++ environments/shadow-mistress/templates/user_data.sh
@@ -6,16 +6,41 @@
 chmod 600 /var/swap.1
 /sbin/swapon /var/swap.1
 
-# Add user
-adduser --disabled-password --gecos \"\" picosushi && sudo adduser picosushi sudo
-sh -c 'echo picosushi\\ ALL=\\(ALL\\)\\ NOPASSWD:ALL > /etc/sudoers.d/picosushi-user'
-
 # Update packages
 export DEBIAN_FRONTEND=noninteractive
 apt update
 apt full-upgrade -y
 apt install -y build-essential git curl apt-transport-https ca-certificates software-properties-common awscli linux-tools-common linux-tools-aws
 apt install -y byobu tmux vim neovim fish jed figlet toilet cmake homesick
+
+# docker
+curl -fsSL https://get.docker.com/ | sudo sh
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+apt update && apt install -y docker-ce
+usermod -aG docker picosushi
+
+# Setup motd
+if hash toilet 2>/dev/null; then
+    echo "${instance_name}" | toilet --font pagga --gay > /etc/motd
+else
+    echo "${instance_name}" > /etc/motd
+fi
+
+# Add user
+adduser --shell /usr/bin/fish --disabled-password --gecos \"\" picosushi && sudo adduser picosushi sudo
+echo 'picosushi ALL=(ALL) NOPASSWD:ALL' | sudo tee -a /etc/sudoers.d/picosushi-user
+
+echo "================User setup================"
+
+# SSH
+mkdir -p /home/picosushi/.ssh
+curl --proto '=https' --tlsv1.2 -sSf -o /home/picosushi/.ssh/authorized_keys https://github.com/PicoSushi.keys
+chown -R picosushi:picosushi /home/picosushi/.ssh
+chmod -R 700 /home/picosushi/.ssh
+
+# Shell
+sudo --login --user=picosushi homesick clone https://github.com/PicoSushi/dotfiles
+yes | sudo --login --user=picosushi homesick link dotfiles
 
 echo "================Install personal packages================"
 # fzf
@@ -26,19 +51,3 @@ yes | sudo --login --user=picosushi /home/picosushi/.fzf/install
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sudo --login --user=picosushi sh -s -- -y
 echo 'export PATH=$${PATH}:$${HOME}/.cargo/bin' | tee -a /home/picosushi/.bashrc
 sudo --login --user=picosushi /home/picosushi/.cargo/bin/cargo install bat exa fd-find ripgrep
-
-# docker
-curl -fsSL https://get.docker.com/ | sudo sh
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-apt update && apt install -y docker-ce
-usermod -aG docker picosushi
-
-echo "================User setup================"
-# SSH
-mkdir -p /home/picosushi/.ssh
-curl --proto '=https' --tlsv1.2 -sSf -o /home/picosushi/.ssh/authorized_keys https://github.com/PicoSushi.keys
-chown -R picosushi:picosushi /home/picosushi/.ssh
-chmod -R 700 /home/picosushi/.ssh
-# Shell
-sudo --login --user=picosushi homesick clone https://github.com/PicoSushi/dotfiles
-yes | sudo --login --user=picosushi homesick link dotfiles
